020d00c4d805149c061b64e27d846cb7710b6d13
LepkoQQ <lepko.san@gmail.com>
2018-12-18 12:05:45 +0100

    allow linking directly to open popups

--- public/script.js
+++ public/script.js
@@ -2,6 +2,10 @@ $(function onReady() {
   var popups = null;
   $.getJSON('popups.json', function onSuccess(data) {
     popups = data;
+    if (autoOpenPopup) {
+      openPopup({ hash: autoOpenPopup });
+      autoOpenPopup = '';
+    }
   });
 
   var $sideText = $('.side-text');
@@ -9,55 +13,99 @@ $(function onReady() {
   var $closeButton = $('.close-button');
   var $letter = $('.letter-container');
 
+  var autoOpenPopup = window.location.hash.slice(1).toLowerCase();
+
+  function changeHash(newHash) {
+    var scr = $(window).scrollTop();
+    window.location.hash = newHash;
+    $('html,body').scrollTop(scr);
+  }
+
+  function closePopup() {
+    $sideText.empty();
+    $letter.find('.aside').removeClass('active');
+
+    $('body').css('overflow', '');
+    $sideTextContainer.css('overflow', '');
+
+    changeHash('');
+  }
+
+  function openPopup(options) {
+    var $this = options.el;
+    var hash = options.hash;
+
+    if (!$this && !hash) {
+      return;
+    }
+
+    var text = null;
+    var popup = null;
+
+    if ($this) {
+      text = $this.text().trim();
+      popup = popups[text];
+    } else if (!$this && hash) {
+      for (prop in popups) {
+        if (popups.hasOwnProperty(prop)) {
+          if (popups[prop].hash === hash) {
+            popup = popups[prop];
+            text = prop;
+            $letter.find('.aside').filter(function(index) {
+              var $el = $(this);
+              if ($el.text().trim() === text) {
+                $this = $el;
+              }
+            });
+            break;
+          }
+        }
+      }
+    }
+
+    if (!popup || !$this) {
+      return;
+    }
+
+    $this.addClass('active');
+
+    $sideText.append($('<h6>').html(popup.title));
+    $sideText.append($('<p>').html(popup.content));
+
+    var top = $this.offset().top - $letter.offset().top;
+    var offset = 50; // $sideText.height() / 2;
+    $sideText.css('margin-top', Math.max(0, top - offset));
+
+    $sideTextContainer.fadeIn('fast');
+    $closeButton.fadeIn('fast');
+
+    if ($sideTextContainer.css('position') === 'fixed') {
+      $('body').css('overflow', 'hidden');
+      $sideTextContainer.css('overflow', 'scroll');
+    }
+
+    changeHash(popup.hash || '');
+  }
+
   $letter.on('click', '.aside', function onClick(event) {
     var $this = $(this);
     var wasActive = $this.hasClass('active');
 
     $sideTextContainer.fadeOut('fast', function fadeDone() {
-      $sideText.empty();
-      $letter.find('.aside').removeClass('active');
-
-      $('body').css('overflow', '');
-      $sideTextContainer.css('overflow', '');
+      closePopup();
 
       if (popups == null || wasActive) {
         return;
       }
 
-      var text = $this.text().trim();
-      var popup = popups[text];
-
-      if (popup == null) {
-        return;
-      }
-
-      $this.addClass('active');
-
-      $sideText.append($('<h6>').html(popup.title));
-      $sideText.append($('<p>').html(popup.content));
-
-      var top = $this.offset().top - $letter.offset().top;
-      var offset = 50; // $sideText.height() / 2;
-      $sideText.css('margin-top', Math.max(0, top - offset));
-
-      $sideTextContainer.fadeIn('fast');
-      $closeButton.fadeIn('fast');
-
-      if ($sideTextContainer.css('position') === 'fixed') {
-        $('body').css('overflow', 'hidden');
-        $sideTextContainer.css('overflow', 'scroll');
-      }
+      openPopup({ el: $this });
     });
   });
 
   $('.close-button').on('click', function onClick() {
     $sideTextContainer.fadeOut('fast');
     $closeButton.fadeOut('fast', function fadeDone() {
-      $sideText.empty();
-      $letter.find('.aside').removeClass('active');
-
-      $('body').css('overflow', '');
-      $sideTextContainer.css('overflow', '');
+      closePopup();
     });
   });
 });
